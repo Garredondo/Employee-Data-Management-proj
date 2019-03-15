3fa7335dee5026aa544ce3453684c0c574235ac9
George Arredondo <arredondo.george@gmail.com>
2019-03-15 12:56:23 -0500

    Updated the repo to include HTML and JS file.

---
+++ program.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyBb6qg_qvaXjCx1WJ-qu-cE5Io_MfY8efQ",
+    authDomain: "this-is-so-cool-31ce5.firebaseapp.com",
+    databaseURL: "https://this-is-so-cool-31ce5.firebaseio.com",
+    projectId: "this-is-so-cool-31ce5",
+    storageBucket: "this-is-so-cool-31ce5.appspot.com",
+    messagingSenderId: "964871781017"
+  };
+  firebase.initializeApp(config);
